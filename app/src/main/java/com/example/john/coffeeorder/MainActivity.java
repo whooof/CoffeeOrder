ee01374fbd37dd14ecda632a984cdf08dd3b1b58
Jan Konieczny <whooof@outlook.com>
2015-09-26 10:59:56 +0100

    Deleted header text, added method for quick Toast generating, simplified some methods. "Thank you" text with the total price now appears after the order is ready.

--- app/src/main/java/com/example/john/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/john/coffeeorder/MainActivity.java
@@ -24,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Quick Toast generator using two params
+     *
+     * @param text   the text of the Toast
+     * @param longer if true then Toast length will be set to long otherwise to short
+     */
+
+    private void toasty(String text, boolean longer) {
+        int length;
+        if (longer) {
+            length = Toast.LENGTH_LONG;
+        } else {
+            length = Toast.LENGTH_SHORT;
+        }
+        Toast.makeText(getApplicationContext(), text, length).show();
+
+    }
+
+    /**
      * This method is called when the order button is clicked.
      */
 
@@ -34,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void subOne(View view) {
         if (quantity == 0) {
-            Toast.makeText(getApplicationContext(), "You can't go below 0!!", Toast.LENGTH_SHORT).show();
+            toasty("You can't go below 0!!", false);
         } else {
             quantity--;
             display(quantity);
@@ -45,40 +63,33 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
 
         if (quantity == 0) {
-            Toast.makeText(getApplicationContext(), "You need to order at least one coffee!", Toast.LENGTH_SHORT).show();
+            toasty("You need to order at least one coffee!", false);
         } else {
             ImageView coffee = (ImageView) findViewById(R.id.coffee);
             coffee.setVisibility(View.VISIBLE);
             TextView summary = (TextView) findViewById(R.id.summary);
-            TextView thankYou = (TextView) findViewById(R.id.thankYou);
-            thankYou.setText("Thank you! ");
             summary.setText("Thanks for ordering we are preparing your ");
             if (quantity == 1) {
                 summary.append("1 coffee");
             } else {
                 summary.append(quantity + " coffees");
             }
-            displayPrice(calculatePrice());
 
             new CountDownTimer(30000 * quantity, 1000) {
 
                 TextView timeLeft = (TextView) findViewById(R.id.timeLeft);
                 Button order = (Button) findViewById(R.id.order);
-                ImageView coffee = (ImageView) findViewById(R.id.coffee);
 
                 public void onTick(long millisUntilFinished) {
                     timeLeft.setText("Your order will be ready in: \n" + millisUntilFinished / 1000 + "s");
                     order.setEnabled(false);
-                    if ((millisUntilFinished / 1000) % 2 == 0) {
-                        coffee.setImageResource(R.drawable.cof1);
-                    } else {
-                        coffee.setImageResource(R.drawable.cof2);
-                    }
+                    animate(millisUntilFinished);
                 }
 
                 public void onFinish() {
                     timeLeft.setText("You can pick up yor order!");
                     order.setEnabled(true);
+                    displayPrice(calculatePrice());
                 }
             }.start();
 
@@ -107,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int price) {
         TextView thankYou = (TextView) findViewById(
                 R.id.thankYou);
-        thankYou.append("\nIt's " + price + "$ in total.");
+        thankYou.setText("Thank you!\nIt's " + price + "$ in total.");
 
     }
     /**
@@ -120,6 +131,21 @@ public class MainActivity extends AppCompatActivity {
                 R.id.display);
         display.setText("" + quantity);
 
+    }
+
+    /**
+     * Method makes a simple animation out of two pictures, change occurs after each second
+     *
+     * @param time how long should it animate it's divided by 1000
+     */
+
+    private void animate(long time) {
+        ImageView coffee = (ImageView) findViewById(R.id.coffee);
+        if ((time / 1000) % 2 == 0) {
+            coffee.setImageResource(R.drawable.cof1);
+        } else {
+            coffee.setImageResource(R.drawable.cof2);
+        }
     }
 
 }
